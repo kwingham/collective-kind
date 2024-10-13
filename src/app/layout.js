cf4492911d9e59ee742c0f457d975f5e70eed794
kwingham <kyle_wingham@outlook.com>
2024-10-13 15:05:00 +0100

    supabase config and base ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,19 +5,42 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
 import "./styles/globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
+          <header>
+            <nav>
+              <ul style={{ display: "flex", gap: "1rem" }}>
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/profile">Profile</Link>
+                </li>
+                <li>
+                  <Link href="/global-timeline">Global Timeline</Link>
+                </li>
+                <li>
+                  <Link href="/notifications">Notifications</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-          {children}
+          <main>{children}</main>
+          <footer>
+            <p>Collective-Kind - Building a kind and supportive community</p>
+          </footer>
         </body>
       </html>
     </ClerkProvider>
